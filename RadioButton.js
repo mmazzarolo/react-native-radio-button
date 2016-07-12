8bc43e286f3e4cf484902efdd20e21504c0673df
mmazzarolo <mazzarolomatteo@gmail.com>
2016-07-12 11:04:23 +0200

    First commit

---
+++ RadioButton.js
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { View } from 'react-native-animatable'
+
+const DEFAULT_SIZE_MULTIPLIER = 0.7
+const DEFAULT_OUTER_BORDER_WIDTH_MULTIPLIER = 0.2
+
+export default class RadioButton extends Component {
+  static propTypes = {
+    size: PropTypes.number,
+    innerColor: PropTypes.string,
+    outerColor: PropTypes.string,
+    isSelected: PropTypes.bool,
+    onPress: PropTypes.func
+  }
+
+  static defaultProps = {
+    size: 16,
+    innerColor: 'dodgerblue',
+    outerColor: 'dodgerblue',
+    isSelected: false,
+    onPress: () => null
+  }
+
+  render () {
+    const { size, innerColor, outerColor, isSelected, onPress } = this.props
+    const outerStyle = {
+      borderColor: outerColor,
+      width: size + size * DEFAULT_SIZE_MULTIPLIER,
+      height: size + size * DEFAULT_SIZE_MULTIPLIER,
+      borderRadius: (size + size * DEFAULT_SIZE_MULTIPLIER) / 2,
+      borderWidth: size * DEFAULT_OUTER_BORDER_WIDTH_MULTIPLIER
+    }
+
+    const innerStyle = {
+      width: size,
+      height: size,
+      borderRadius: size / 2,
+      backgroundColor: innerColor
+    }
+
+    return (
+      <TouchableOpacity style={[styles.radio, outerStyle]} onPress={onPress}>
+        {isSelected ? <View style={innerStyle} {...this.props} /> : null}
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  radio: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center'
+  }
+})
